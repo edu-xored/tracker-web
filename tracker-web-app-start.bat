3e8d3ce3c9657efe0f8a74af5bb0859af78085a5
Роман Бабков < >
2016-10-21 20:36:12 +0700

    Handle java run errors
    Card: #3

--- tracker-web-app-start.bat
+++ tracker-web-app-start.bat
@@ -1,9 +1,13 @@
 @echo off
-if "%1"=="" (echo No parameters, set path to the file. & set /p filepath="Path: ") else (set filepath=%1)
-if not exist %filepath% (@echo File not found. & pause > nul & goto end) 
+set log=tracker-web-app.log
+if "%1"=="" (echo No parameters, set path to the file & set /p filepath="Path: ") else (set filepath=%1)
+if not exist %filepath% (echo File not found. & pause > nul & goto end) 
 echo Starting server...
-start "tracker-web-app" /i /min cmd /c "java -jar %filepath% > tracker-web-app.log"
-timeout /T 6 /NOBREAK > nul
+start "tracker-web-app" /i /min cmd /c "java -jar %filepath% >%log% 2>&1"
+timeout /T 1 /NOBREAK > nul
+findstr /B "Error:" %log%
+if %ERRORLEVEL%==0 (pause > nul & goto end)
+timeout /T 5 /NOBREAK > nul
 start http://localhost:8080
 echo Press any button to quit
 pause > nul
