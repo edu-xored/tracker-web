c741e1451f6ffeec365df31a5d6808f1d67a4fbc
Dmitry Gerasimenko <astronautr@gmail.com>
2016-10-21 19:07:44 +0700

    Add quotes to path variables, redirect stderr to standart .log file
    For redirecting &>> was used, cause 2>&1 >> (like in prev. version) doesn't work.
    
    Card: #3

--- tracker-web-app-start.sh
+++ tracker-web-app-start.sh
@@ -5,8 +5,8 @@
 #into tracker-web-app.log file.
 
 checkServerIsAlive () {
-	if [[ $(ps | grep -o $PROC_ID) -ne "$PROC_ID" ]]; then
-		echo "Process killed"
+	if [[ $(ps | grep -o $PROC_ID) -ne $PROC_ID ]]; then
+		echo "Server killed"
 		return 1
 	fi
 	return 0
@@ -15,16 +15,24 @@ checkServerIsAlive () {
 PATH_TO_JAR=$1
 LOG_FILE="tracker-web-app.log"
 
-if !(test -e $PATH_TO_JAR); then
+if !(test -e "$PATH_TO_JAR"); then
 	echo "File not found"
 	exit 1
 fi
 
-echo "Starting the server"
-echo -e "\nStarting the server\n" 2>&1 >> $LOG_FILE
-
-java -jar $PATH_TO_JAR >> $LOG_FILE &
+java -jar "$PATH_TO_JAR" &>> "$LOG_FILE" &
 PROC_ID=$!
+
+checkServerIsAlive
+SERVER_ALIVE=$?
+
+if [[ $SERVER_ALIVE == 0 ]]; then
+	echo "Starting the server"
+	echo -e "\nStarting the server\n" &>> "$LOG_FILE"
+else
+	echo "Error while trying to start local server"
+	exit 1
+fi
 
 while [[ $(curl -sL -w "%{http_code}\\n" http://localhost:8080 -o /dev/null) -eq "000" ]];
 do
@@ -42,10 +50,9 @@ xdg-open http://localhost:8080 &
 echo "Press any button to quit"
 read -s -n 1
 echo "Killing the server"
-kill $PROC_ID
+kill "$PROC_ID"
 
-checkServerIsAlive
-SERVER_ALIVE=$?
+SERVER_ALIVE=0
 
 while [[ $SERVER_ALIVE == 0 ]]; 
 do
@@ -55,5 +62,5 @@ do
 done
 
 echo "Server stopped"
-echo -e "\nServer stopped\n" >> $LOG_FILE
+echo -e "\nServer stopped\n" &>> "$LOG_FILE"
 exit 0
