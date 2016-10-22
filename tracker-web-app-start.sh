f925f57dcda2c4d253b6e553b65ed29364147b71
Dmitry Gerasimenko <astronautr@gmail.com>
2016-10-22 16:51:59 +0700

    Add processing of boundary case and fixes in variable naming
    Now script can process case when no argument provided or more than one argument provided. Local variables
    writes in lower case.
    
    Card: #3

--- tracker-web-app-start.sh
+++ tracker-web-app-start.sh
@@ -5,40 +5,61 @@
 #into tracker-web-app.log file.
 
 checkServerIsAlive () {
-	if [[ $(ps | grep -o $PROC_ID) -ne $PROC_ID ]]; then
+	if [[ $(ps | grep -o $proc_id) -ne $proc_id ]]; then
 		echo "Server killed"
 		return 1
 	fi
 	return 0
 }
 
-PATH_TO_JAR=$1
-LOG_FILE="tracker-web-app.log"
+arg_number=$#
+
+if [[ $arg_number -eq 0 ]]; then
+	echo "Script argument not found, please specify the path to tracker-web-app jar file"
+	read PATH_TO_JAR
+elif [[ $arg_number -eq 1 ]]; then
+	PATH_TO_JAR=$1
+else
+	echo "Unallowable number of arguments"
+	echo "Press q to exit, Space or Return to specify the path to tracker-web-app jar file"
+	PRESSED_KEY="null"
+	while [[ "$PRESSED_KEY" != "q" && "$PRESSED_KEY" != "$(echo -e $'\n')" ]]; do
+		read -s -n 1 PRESSED_KEY
+	done
+	if [[ $PRESSED_KEY == "q" ]]; then
+		echo "Closing the program"
+		exit 0
+	elif [[ $PRESSED_KEY == "$(echo -e $'\n')" ]]; then
+		echo -e "\nSpecify the path to tracker-web-app jar file"
+		read PATH_TO_JAR
+	fi
+fi
+
+log_file="tracker-web-app.log"
 
 if !(test -e "$PATH_TO_JAR"); then
 	echo "File not found"
 	exit 1
 fi
 
-java -jar "$PATH_TO_JAR" &>> "$LOG_FILE" &
-PROC_ID=$!
+java -jar "$PATH_TO_JAR" &>> "$log_file" &
+proc_id=$!
 
 checkServerIsAlive
-SERVER_ALIVE=$?
+server_alive=$?
 
-if [[ $SERVER_ALIVE == 0 ]]; then
+if [[ $server_alive -eq 0 ]]; then
 	echo "Starting the server"
-	echo -e "\nStarting the server\n" &>> "$LOG_FILE"
+	echo -e "\nStarting the server\n" &>> "$log_file"
 else
 	echo "Error while trying to start local server"
 	exit 1
 fi
 
-while [[ $(curl -sL -w "%{http_code}\\n" http://localhost:8080 -o /dev/null) -eq "000" ]];
-do
+while [[ $(curl -sL -w "%{http_code}\\n" http://localhost:8080 -o /dev/null) -eq 000 ]]; do
 	checkServerIsAlive
-	SERVER_ALIVE=$?
-	if [[ $SERVER_ALIVE == 1 ]]; then
+	server_alive=$?
+	if [[ $server_alive -eq 1 ]]; then
 		echo "Error while trying to start local server"
 		exit 1
 	fi
@@ -50,17 +71,16 @@ xdg-open http://localhost:8080 &
 echo "Press any button to quit"
 read -s -n 1
 echo "Killing the server"
-kill "$PROC_ID"
+kill $proc_id
 
-SERVER_ALIVE=0
+server_alive=0
 
-while [[ $SERVER_ALIVE == 0 ]]; 
-do
+while [[ $server_alive -eq 0 ]]; do
 	sleep 1
 	checkServerIsAlive
-	SERVER_ALIVE=$?
+	server_alive=$?
 done
 
 echo "Server stopped"
-echo -e "\nServer stopped\n" &>> "$LOG_FILE"
+echo -e "\nServer stopped\n" &>> "$log_file"
 exit 0
