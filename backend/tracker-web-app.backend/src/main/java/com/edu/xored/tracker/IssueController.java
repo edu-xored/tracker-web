31ad39fdab00ff6f6d7727a610417f67324f0a35
Shabanov Artem <aashabanov758@gmail.com>
2016-10-23 21:28:10 +0700

    Remove unused variable hashCount, fixed GET request status
    If we try to put element form HashMap and element with this
    key doesn't exist returns 404 Not Found status
    
    Card: #21

--- backend/tracker-web-app.backend/src/main/java/com/edu/xored/tracker/IssueController.java
+++ backend/tracker-web-app.backend/src/main/java/com/edu/xored/tracker/IssueController.java
@@ -2,7 +2,10 @@ package com.edu.xored.tracker;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -10,34 +13,33 @@ import java.util.Map;
 @RestController
 @RequestMapping(value = {"/issue", "/issue/{hash}"})
 public class IssueController {
-
+    // TODO: temporary issues storage for testing purposes. Remove when implement access to real issues through CLI core.
     private static Map<Long, Issue> issueMap = new HashMap<Long, Issue>();
-    private static long hashCounter;
 
     static {
-
         Issue firstIssue = new Issue(
-                hashCounter++,
+                0L,
                 "First issue",
                 "First issue's description",
                 Issue.Status.OPEN
         );
 
         Issue secondIssue = new Issue(
-                hashCounter++,
+                1L,
                 "Second issue",
                 "Second issue's description",
                 Issue.Status.CLOSED
         );
 
-
         issueMap.put(firstIssue.getHash(), firstIssue);
         issueMap.put(secondIssue.getHash(), secondIssue);
-
     }
 
     @GetMapping
     public ResponseEntity<Issue> get(@PathVariable("hash") long hash) {
+        if (!issueMap.containsKey(hash)) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
+        }
         return ResponseEntity.status(HttpStatus.OK).body(issueMap.get(hash));
     }
 }
