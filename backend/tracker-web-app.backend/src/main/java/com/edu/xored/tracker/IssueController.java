1b2ca05933fa08cfbc901288ca174f5e4fbbd194
Shabanov Artem <aashabanov758@gmail.com>
2016-10-24 12:09:39 +0700

    Delete unsed imports
    Card: #21

--- backend/tracker-web-app.backend/src/main/java/com/edu/xored/tracker/IssueController.java
+++ backend/tracker-web-app.backend/src/main/java/com/edu/xored/tracker/IssueController.java
@@ -1,7 +1,5 @@
 package com.edu.xored.tracker;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
