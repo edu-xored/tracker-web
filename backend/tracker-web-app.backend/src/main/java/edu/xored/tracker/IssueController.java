d55dbc3fe683c76d675e92b64c6d5444b64cff5c
Vladimir Pleshkov <pleshkovvli@gmail.com>
2016-10-26 00:35:33 +0700

    GET-endpoints for issues by status

--- backend/tracker-web-app.backend/src/main/java/edu/xored/tracker/IssueController.java
+++ backend/tracker-web-app.backend/src/main/java/edu/xored/tracker/IssueController.java
@@ -3,7 +3,10 @@ package edu.xored.tracker;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.*;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.List;
+import java.util.ArrayList;
 
 @RestController
 @RequestMapping(value = "/issues")
@@ -17,7 +20,6 @@ public class IssueController {
                 "First issue",
                 "First issue's description",
                 Issue.Status.OPEN
-
         );
 
         Issue secondIssue = new Issue(
@@ -57,61 +59,17 @@ public class IssueController {
     }
 
     @GetMapping(params = {"status"})
-    public List<Issue> getIssuesByStatus(@RequestParam("status") Issue.Status status){
+    public List<Issue> getIssuesByStatus(@RequestParam("status") Issue.Status status) {
         List<Issue> statusIssueList = new ArrayList<Issue>();
-        for(Map.Entry<Long, Issue> entry : issueMap.entrySet()){
-            if(status==entry.getValue().getStatus()){
+        for(Map.Entry<Long, Issue> entry : issueMap.entrySet()) {
+            if(status==entry.getValue().getStatus()) {
                 statusIssueList.add(entry.getValue());
             }
         }
-        if(statusIssueList.isEmpty()){
-            throw new IssueNotFoundStatusException();
-        }
         return statusIssueList;
-    }
-
-    @GetMapping(params = {"description"})
-    public List<Issue> getIssuesByDescription(@RequestParam("description") String description){
-        List<Issue> descriptionIssueList = new ArrayList<Issue>();
-        for(Map.Entry<Long, Issue> entry : issueMap.entrySet()){
-            if(entry.getValue().getDescription().contains(description)){
-                descriptionIssueList.add(entry.getValue());
-            }
-        }
-        if(descriptionIssueList.isEmpty()){
-            throw new IssueNotFoundDescriptionException();
-        }
-        return descriptionIssueList;
-    }
-
-    @GetMapping(params = {"summary"})
-    public List<Issue> getIssuesBySummary(@RequestParam("summary") String summary){
-        List<Issue> summaryIssueList = new ArrayList<Issue>();
-        for(Map.Entry<Long, Issue> entry : issueMap.entrySet()){
-            if(entry.getValue().getSummary().contains(summary)){
-                summaryIssueList.add(entry.getValue());
-            }
-        }
-        if(summaryIssueList.isEmpty()){
-            throw new IssueNotFoundSummaryException();
-        }
-        return summaryIssueList;
     }
 
     @ResponseStatus(value = HttpStatus.NOT_FOUND, reason = "Issue not found")
     private class IssueNotFoundException extends RuntimeException {
     }
-
-    @ResponseStatus(value = HttpStatus.NOT_FOUND, reason = "Status not found")
-    private class IssueNotFoundStatusException extends RuntimeException {
-    }
-
-    @ResponseStatus(value = HttpStatus.NOT_FOUND, reason = "Description not found")
-    private class IssueNotFoundDescriptionException extends RuntimeException {
-    }
-
-    @ResponseStatus(value = HttpStatus.NOT_FOUND, reason = "Summary not found")
-    private class IssueNotFoundSummaryException extends RuntimeException {
-    }
-
 }
