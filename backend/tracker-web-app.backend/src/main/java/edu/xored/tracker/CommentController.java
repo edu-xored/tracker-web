bac3c205f60dd21296981712fdebe1a31fa24060
Роман Бабков < >
2016-10-27 19:38:26 +0700

    Change name of variable hash to issueHash
    Card: #28

--- backend/tracker-web-app.backend/src/main/java/edu/xored/tracker/CommentController.java
+++ backend/tracker-web-app.backend/src/main/java/edu/xored/tracker/CommentController.java
@@ -4,8 +4,8 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class CommentController {
-    @PostMapping(value = "/issues/{hash}/comments")
-    public void post(@RequestBody Comment comment, @PathVariable("hash") long hash) {
+    @PostMapping(value = "/issues/{issueHash}/comments")
+    public void post(@RequestBody Comment comment, @PathVariable("issueHash") long hash) {
         IssueController issueController = new IssueController();
         Issue issue = issueController.getIssue(hash);
         comment.setCurrentTime();
