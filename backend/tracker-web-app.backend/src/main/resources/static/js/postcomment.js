e3b97965cedc9e6e3c245548b98c326b9a77398a
Роман Бабков < >
2016-10-28 02:33:16 +0700

    Alerting then fields Number of issue and comment are empty
    Card: #28

--- backend/tracker-web-app.backend/src/main/resources/static/js/postcomment.js
+++ backend/tracker-web-app.backend/src/main/resources/static/js/postcomment.js
@@ -3,6 +3,14 @@ postApp.controller('postController', function($scope, $http)
 {
 	$scope.comment = {};
 	$scope.submitForm = function() {
+		if ($scope.issueHash == undefined) {
+			alert( 'Enter a number of issue!' );
+			return;
+		}
+		if (!$scope.comment.content) {
+			alert( 'Enter a comment!' );
+			return;
+		}
 	    if (!$scope.comment.author) {
                 $scope.comment.author = "Anonymous";
         }
