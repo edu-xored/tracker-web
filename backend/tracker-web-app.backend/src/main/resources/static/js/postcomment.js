20a655cb62bb007beff8e7ba206ac9f465da169d
Роман Бабков < >
2016-10-28 03:09:13 +0700

    Add status of comment and you can see some errors
    Card: #28

--- backend/tracker-web-app.backend/src/main/resources/static/js/postcomment.js
+++ backend/tracker-web-app.backend/src/main/resources/static/js/postcomment.js
@@ -4,11 +4,11 @@ postApp.controller('postController', function($scope, $http)
 	$scope.comment = {};
 	$scope.submitForm = function() {
 		if ($scope.issueHash == undefined) {
-			alert( 'Enter a number of issue!' );
+			$scope.status = 'Enter a number of issue!';
 			return;
 		}
 		if (!$scope.comment.content) {
-			alert( 'Enter a comment!' );
+			$scope.status = 'Enter a comment!';
 			return;
 		}
 	    if (!$scope.comment.author) {
@@ -18,6 +18,12 @@ postApp.controller('postController', function($scope, $http)
 			method	:	'POST',
 			url	:	'/issues/' + $scope.issueHash + '/comments',
 			data	:	$scope.comment
-		});
+		})
+			.success(function() {
+				$scope.status = 'Comment posted.';
+			})
+			.error(function() {
+				$scope.status = 'Comment didn\'t poste.';
+			});
 	};
 });
