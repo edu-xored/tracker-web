6a121cea8bb477be5fb7ed976523e5c2c6326e4a
Роман Бабков < >
2016-10-29 03:58:01 +0700

    Delete all set methods from Comment
    Card: #28

--- backend/tracker-web-app.backend/src/main/java/edu/xored/tracker/Comment.java
+++ backend/tracker-web-app.backend/src/main/java/edu/xored/tracker/Comment.java
@@ -23,16 +23,8 @@ public class Comment {
         return content;
     }
 
-    public void setContent(String content) {
-        this.content = content;
-    }
-
     public String getAuthor() {
         return author;
-    }
-
-    public void setAuthor(String author) {
-        this.author = author;
     }
 
     public LocalDateTime getCreatedDateTime() {
