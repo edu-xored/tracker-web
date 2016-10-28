8273d043e43a0ac1f7fc9ab3227fb6508640e7e2
Dmitry Gerasimenko <astronautr@gmail.com>
2016-10-28 10:35:42 +0700

    Specify each import istead of using .*
    Card: #37

--- backend/tracker-web-app.backend/src/main/java/edu/xored/tracker/IssueController.java
+++ backend/tracker-web-app.backend/src/main/java/edu/xored/tracker/IssueController.java
@@ -3,7 +3,10 @@ package edu.xored.tracker;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping(value = "/issues")
