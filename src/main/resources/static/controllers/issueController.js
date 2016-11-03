3a47a4808450a21e73bd0b47406297711beb67af
Dmitry Gerasimenko <astronautr@gmail.com>
2016-11-03 15:26:19 +0700

    Introduce routing to specific issue from all issues table
    Issue controller was reorganized: add $routeParams instead of parsing logic, replace controllerAs syntax with $scope. Also add appropriate changes in other needed files.
    
    Card :#5

--- src/main/resources/static/controllers/issueController.js
+++ src/main/resources/static/controllers/issueController.js
@@ -1,26 +1,12 @@
-angular.module('issueApp', [])
-    .controller('issueController', function($location,$http){
-        var issue = this;
-        var url=$location.absUrl();
-        var paramString = "?hash=";
-        var hash="";
-        lastIndex=url.lastIndexOf(paramString);
-        if(lastIndex!=-1) {
-            hash=url.slice(lastIndex + paramString.length);
-        }
-        if(hash.length<20 && hash.match(/^[0-9]+$/)) {
-           $http({
-           method: 'GET',
-           url: "/issues/" + hash
-           }).then(function successCallback(response) {
-                issue.summary=response.data.summary;
-                issue.description=response.data.description;
-                issue.hash=response.data.hash;
-                }, function errorCallback(response) {
-                issue.error = response.data.message;
-                });
-         }
-         else {
-         issue.error = "Wrong param";
-         }
-    });
+angular.module('trackerWebApp')
+  .controller('issueController', function($scope, $http, $routeParams) {
+    $http({
+       method: 'GET',
+       url: "/issues/" + $routeParams.hash
+     })
+     .then(function(response) {
+        $scope.issue = response.data;
+      }, function(response) {
+          $scope.error = response.status.Text;
+      });
+});
--- src/main/resources/static/trackerWebApp.routes.js
+++ src/main/resources/static/trackerWebApp.routes.js
@@ -2,6 +2,10 @@ angular
   .module('trackerWebApp')
   .config(function($routeProvider, $locationProvider) {
     $routeProvider
+      .when('/issues/:hash', {
+        templateUrl: '/views/issueView.html',
+        controller: 'issueController'
+      })
       .otherwise({
         templateUrl: '/views/allIssuesView.html',
         controller: 'issuesController'
