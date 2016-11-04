57c120703d18395bb64aab48eff580844fccebcf
Artem Shabanov <aashabanov758@gmail.com>
2016-11-05 00:06:37 +0700

    Add simple filter for display only open issues or only closed issues
    Card: #14

--- src/main/resources/static/controllers/allIssuesController.js
+++ src/main/resources/static/controllers/allIssuesController.js
@@ -1,9 +1,11 @@
 angular.module('trackerWebApp')
 	.controller('issuesController', function($scope, $http) {
-	$http.get('/issues').
-	then(function(response) {
-		$scope.issues = response.data;
-	}, function(response) {
-		$scope.error = response.status.Text;
-	});
+		$scope.selectedStatus = "";
+		$scope.statusVariants = ["", "OPEN", "CLOSED"];
+		$http.get('/issues').
+		then(function(response) {
+			$scope.issues = response.data;
+		}, function(response) {
+			$scope.error = response.status.Text;
+		});
 });
