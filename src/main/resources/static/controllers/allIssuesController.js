032ec030a18415a9530db5b3bd2c7fd12335c919
Artem Shabanov <aashabanov758@gmail.com>
2016-11-05 02:28:55 +0700

    Add sorting in allIssues by hash
    Available ascending & descending sorting
    
    Card: #14

--- src/main/resources/static/controllers/allIssuesController.js
+++ src/main/resources/static/controllers/allIssuesController.js
@@ -2,10 +2,20 @@ angular.module('trackerWebApp')
 	.controller('issuesController', function($scope, $http) {
 		$scope.selectedStatus = "";
 		$scope.statusVariants = ["", "OPEN", "CLOSED"];
+		$scope.sortField = undefined;
+		$scope.reverse = false;
 		$http.get('/issues').
 		then(function(response) {
 			$scope.issues = response.data;
 		}, function(response) {
 			$scope.error = response.status.Text;
 		});
+		$scope.sort = function(fieldName) {
+			if ($scope.sortField == fieldName) {
+				$scope.reverse = !$scope.reverse;
+			} else {
+				$scope.sortField = fieldName;
+				$scope.reverse = false;
+			}
+		};
 });
