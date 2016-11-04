96aebb380697809dfa687bfc8deb8f8b54c81bbd
Declow0 <declow@mail.ru>
2016-11-04 10:06:52 +0700

    Add pagination on all Issue page
    Card: #15

--- src/main/resources/static/controllers/allIssuesController.js
+++ src/main/resources/static/controllers/allIssuesController.js
@@ -1,9 +1,22 @@
 angular.module('trackerWebApp')
-  .controller('issuesController', function($scope, $http) {
-      $http.get('/issues').
-      then(function(response) {
-          $scope.issues = response.data;
-      }, function(response) {
-          $scope.error = response.status.Text;
-      });
-});
+	.controller('issuesController', function($scope, $http) {
+		$scope.currentPage = 0;
+    	$scope.pageSize = 10;
+    	$scope.pageSizeSelects = [2, 5, 10, 20, 50];
+    	$scope.issues = [];
+    	$http.get('/issues').
+    	then(function(response) {
+        	$scope.issues = response.data;
+    	}, function(response) {
+        	$scope.error = response.status.Text;
+    	});
+    	$scope.numberOfPages = function() {
+        	return Math.ceil($scope.issues.length / $scope.pageSize);
+    	};
+	})
+	.filter('startFrom', function() {
+    	return function(input, start) {
+        	start = +start; //parse to int
+        	return input.slice(start);
+    	}
+	});
