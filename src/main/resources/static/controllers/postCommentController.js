9c46a0d250ae7d6b5f99405d4e735a0deadf018a
Dmitry Gerasimenko <astronautr@gmail.com>
2016-11-05 01:03:50 +0700

    Introduce completed postComment feature
    Refactor all mentions of controller name, change path to controller in index.html,
    add link to postCommentView to single issue view, del hash input field in postCommentView.
    Also, fix initialization of module in controller.
    
    Card: #26

--- src/main/resources/static/controllers/postCommentController.js
+++ src/main/resources/static/controllers/postCommentController.js
@@ -1,21 +1,21 @@
-angular.module('trackerWebApp', [])
-.postApp.controller('postCommentController', function($scope, $http, $routeParams) {
-	$scope.comment = {};
-	$scope.submitForm = function() {
-		if (!$scope.comment.content) {
-			$scope.status = 'Enter a comment!';
-			return;
-		}
-		$http({
-			method	:	'POST',
-			url	:	'/issues/' + $routeParams.hash + '/comments',
-			data	:	$scope.comment
-		})
-			.success(function() {
-				$scope.status = 'Comment posted.';
+angular.module('trackerWebApp')
+	.controller('postCommentController', function($scope, $http, $routeParams) {
+		$scope.comment = {};
+		$scope.submitForm = function() {
+			if (!$scope.comment.content) {
+				$scope.status = 'Enter a comment!';
+				return;
+			}
+			$http({
+				method	:	'POST',
+				url	:	'/issues/' + $routeParams.hash + '/comments',
+				data	:	$scope.comment
 			})
-			.error(function() {
-				$scope.status = 'Comment didn\'t post.';
-			});
-	};
+				.success(function() {
+					$scope.status = 'Comment posted.';
+				})
+				.error(function() {
+					$scope.status = 'Comment didn\'t post.';
+				});
+		};
 });
