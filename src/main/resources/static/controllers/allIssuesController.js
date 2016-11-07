dd747090df45082efae0b61dc49fcabf3b63cb7e
Artem Shabanov <aashabanov758@gmail.com>
2016-11-07 23:19:46 +0700

    Sort by hash removed
    Card: #14

--- src/main/resources/static/controllers/allIssuesController.js
+++ src/main/resources/static/controllers/allIssuesController.js
@@ -8,14 +8,4 @@ angular.module('trackerWebApp')
 		});
 		$scope.selectedStatus = "";
 		$scope.statusVariants = ["", "OPEN", "CLOSED"];
-		$scope.sortField = undefined;
-		$scope.reverse = false;
-		$scope.sort = function(fieldName) {
-			if ($scope.sortField == fieldName) {
-				$scope.reverse = !$scope.reverse;
-			} else {
-				$scope.sortField = fieldName;
-				$scope.reverse = false;
-			}
-		};
 	});
