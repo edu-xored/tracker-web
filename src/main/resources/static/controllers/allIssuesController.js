fa69da512d5641e97a8f123a00d1d76d7d5c6763
Artem Shabanov <aashabanov758@gmail.com>
2016-11-07 23:10:32 +0700

    Resolve conflicts with master allIssuesController and allIssuesView
    Card: #14

--- src/main/resources/static/controllers/allIssuesController.js
+++ src/main/resources/static/controllers/allIssuesController.js
@@ -1,15 +1,15 @@
 angular.module('trackerWebApp')
 	.controller('issuesController', function($scope, $http) {
-		$scope.selectedStatus = "";
-		$scope.statusVariants = ["", "OPEN", "CLOSED"];
-		$scope.sortField = undefined;
-		$scope.reverse = false;
-		$http.get('/issues').
+		$http.get('api/issues').
 		then(function(response) {
 			$scope.issues = response.data;
 		}, function(response) {
 			$scope.error = response.status.Text;
 		});
+		$scope.selectedStatus = "";
+		$scope.statusVariants = ["", "OPEN", "CLOSED"];
+		$scope.sortField = undefined;
+		$scope.reverse = false;
 		$scope.sort = function(fieldName) {
 			if ($scope.sortField == fieldName) {
 				$scope.reverse = !$scope.reverse;
