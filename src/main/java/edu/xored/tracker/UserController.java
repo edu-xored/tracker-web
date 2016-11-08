644c9c933e649422fe54c0dc34dbe8bce2c98d1c
Declow0 <declow@mail.ru>
2016-11-08 23:02:17 +0700

    Fixed bugs in code
    Change import, now without java.io.*. Add method getOutput() to get data from git command. Add 2 constants of commands. Now for work with buffer using try-with-resources. Change exception's description for Output. Add TODO comment.
    
    Card: #32

--- src/main/java/edu/xored/tracker/UserController.java
+++ src/main/java/edu/xored/tracker/UserController.java
@@ -4,45 +4,37 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
-import java.io.*;
 
 @RestController
 public class UserController {
+    private static String getNameCommand = "git config user.name";
+    private static String getEmailCommand = "git config user.email";
 
-    @GetMapping(value = "/user")
+    String getOutput (String command) {
+        Process theProcess;
+        // TODO: How to work with exceptions? How to log them?
+        try {
+            theProcess = Runtime.getRuntime().exec(command);
+        } catch (IOException e) {
+            e.printStackTrace();
+            throw new GitNotFoundException();
+        }
+        try (BufferedReader inStream = new BufferedReader(new InputStreamReader( theProcess.getInputStream() ))) {
+            return inStream.readLine();
+        } catch(IOException e) {
+            e.printStackTrace();
+            throw new ReceiveOutputException();
+        }
+    }
+
+    @GetMapping(value = "/api/user")
     public User getUser() {
-        Process theProcess = null;
-        BufferedReader inStream = null;
-        String name = null;
-        String email = null;
-        try {
-            theProcess = Runtime.getRuntime().exec("git config user.name");
-        } catch (IOException e) {
-            e.printStackTrace();
-            throw new GitNotFoundException();
-        }
-        try {
-            inStream = new BufferedReader(new InputStreamReader( theProcess.getInputStream() ));
-            name = inStream.readLine();
-        } catch(IOException e) {
-            e.printStackTrace();
-            throw new InSrteamException();
-        }
-
-        try {
-            theProcess = Runtime.getRuntime().exec("git config user.email");
-        } catch (IOException e) {
-            e.printStackTrace();
-            throw new GitNotFoundException();
-        }
-        try {
-            inStream = new BufferedReader(new InputStreamReader( theProcess.getInputStream() ));
-            email = inStream.readLine();
-        } catch(IOException e) {
-            e.printStackTrace();
-            throw new InSrteamException();
-        }
+        String name = getOutput(getNameCommand);
+        String email = getOutput(getEmailCommand);
         return new User(name, email);
     }
 
@@ -50,7 +42,7 @@ public class UserController {
     private class GitNotFoundException extends RuntimeException {
     }
 
-    @ResponseStatus(value = HttpStatus.FAILED_DEPENDENCY, reason = "Error on inStream.readLine()")
-    private class InSrteamException extends RuntimeException {
+    @ResponseStatus(value = HttpStatus.FAILED_DEPENDENCY, reason = "An error occurred while receiving the output")
+    private class ReceiveOutputException extends RuntimeException {
     }
 }
