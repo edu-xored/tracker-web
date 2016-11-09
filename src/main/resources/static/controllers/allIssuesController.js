2c8eae1ef8aa7e4f065f2f8aae9aea2352e77ad2
Declow0 <declow@mail.ru>
2016-11-09 20:01:41 +0700

    Second version of pagination
    Now search works correctly!
    
    Card: #15

--- src/main/resources/static/controllers/allIssuesController.js
+++ src/main/resources/static/controllers/allIssuesController.js
@@ -1,18 +1,45 @@
 angular.module('trackerWebApp')
 	.controller('issuesController', function($scope, $http) {
 		$scope.currentPage = 0;
-		$scope.pageSize = 10;
-		$scope.pageSizeSelects = [2, 5, 10, 20, 50];
+		$scope.itemsPerPage = 10;
+		$scope.itemsPerPageSelects = [2, 5, 10, 20, 50];
 		$scope.issues = [];
+		$scope.firstPage = function() {
+            return $scope.currentPage == 0;
+        }
+        $scope.lastPage = function() {
+            var lastPageNum = Math.ceil($scope.filtered.length / $scope.itemsPerPage - 1);
+            return $scope.currentPage == lastPageNum;
+        }
+        $scope.numberOfPages = function(){
+            return Math.ceil($scope.filtered.length / $scope.itemsPerPage);
+        }
+        $scope.startingItem = function() {
+            return $scope.currentPage * $scope.itemsPerPage;
+        }
+        $scope.pageBack = function() {
+            $scope.currentPage = $scope.currentPage - 1;
+        }
+        $scope.pageForward = function() {
+            $scope.currentPage = $scope.currentPage + 1;
+        }
+        $scope.changeItemsPerPage = function() {
+            $scope.currentPage = Math.floor($scope.currentPage * $scope.itemsPerPage / $scope.itemsPerPageSelect);
+            $scope.itemsPerPage = $scope.itemsPerPageSelect;
+        }
+        $scope.initItemsPerPage = function() {
+            $scope.itemsPerPageSelect = $scope.itemsPerPage;
+        }
+        $scope.filteredPagination = function() {
+            $scope.currentPage = 0;
+        }
 		$http.get('/issues').
 		then(function(response) {
 			$scope.issues = response.data;
+			$scope.itemsSize = $scope.issues.length;
 		}, function(response) {
 			$scope.error = response.status.Text;
 		});
-		$scope.numberOfPages = function() {
-			return Math.ceil($scope.issues.length / $scope.pageSize);
-		};
 	})
 	.filter('startFrom', function() {
 		return function(input, start) {
