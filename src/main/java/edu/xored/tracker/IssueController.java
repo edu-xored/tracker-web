a55d2d3dc812ff1260683e7efcbcd6300ea570f2
zaNudda <a.pryah@gmail.com>
2016-11-19 17:33:27 +0700

    Add dummy implementation of in-memory IssueRepository
    Card: #8

--- src/main/java/edu/xored/tracker/IssueController.java
+++ src/main/java/edu/xored/tracker/IssueController.java
@@ -4,119 +4,68 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Collection;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
 import java.util.stream.Collectors;
+import java.util.stream.StreamSupport;
 
 @RestController
 @RequestMapping(value = "/api/issues")
 public class IssueController {
-    // TODO: temporary issues storage for testing purposes. Remove when implement access to real issues through CLI core.
-    private static Map<Long, Issue> issueMap = new HashMap<Long, Issue>();
 
-    static {
-        Issue firstIssue = new Issue(
-                0L,
-                "First issue",
-                "First issue's description",
-                Issue.Status.OPEN
-        );
-        firstIssue.addComment(new Comment("some author", "some content"));
-
-        Issue secondIssue = new Issue(
-                1L,
-                "Second issue",
-                "Second issue's description",
-                Issue.Status.CLOSED
-        );
-        secondIssue.addComment(new Comment("another author", "another content"));
-
-        Issue thirdIssue = new Issue(
-                2L,
-                "Third issue",
-                "Third issue's description",
-                Issue.Status.OPEN
-        );
-
-        Issue fourthIssue = new Issue(
-                3L,
-                "Fourth issue",
-                "Fourth issue's description",
-                Issue.Status.CLOSED
-        );
-
-        issueMap.put(firstIssue.getHash(), firstIssue);
-        issueMap.put(secondIssue.getHash(), secondIssue);
-        issueMap.put(thirdIssue.getHash(), thirdIssue);
-        issueMap.put(fourthIssue.getHash(), fourthIssue);
-    }
+    private IssueRepository issueRepository = new IssueRepositoryImpl();
 
     @PostMapping
     public Issue postIssue(@RequestBody Issue issue) {
-        issue.setHash(issueMap.size());
-        issueMap.put(issue.getHash(), issue);
-        return issue;
+        return issueRepository.save(issue);
     }
 
     @GetMapping(value = "/{hash}")
     public Issue getIssue(@PathVariable("hash") long hash) {
-        Issue issue = issueMap.get(hash);
-        if (issue == null) {
-            throw new IssueNotFoundException();
-        }
-        return issue;
+        throwIfIssueNotFound(hash);
+
+        return issueRepository.findOne(hash);
     }
 
     @PutMapping(value = "/{hash}")
     public Issue putIssue(@PathVariable("hash") long hash,
                           @RequestBody Issue issue) {
-        if (!issueMap.containsKey(hash)) {
-            throw new IssueNotFoundException();
-        }
-        List<Comment> comments = issueMap.get(hash).getComments();
-        issue.setHash(hash);
-        issueMap.put(hash, issue);
-        issue.addComments(comments);
-        return issue;
+        throwIfIssueNotFound(hash);
+
+        return issueRepository.replace(hash, issue);
     }
 
     @DeleteMapping(value = "/{hash}")
     public void deleteIssue(@PathVariable("hash") long hash) {
-        if (!issueMap.containsKey(hash)) {
-            throw new IssueNotFoundException();
-        }
-        issueMap.remove(hash);
+        throwIfIssueNotFound(hash);
+
+        issueRepository.delete(hash);
     }
 
     @PatchMapping(value = "/{hash}")
     public Issue patchIssue(@PathVariable("hash") long hash,
                             @RequestBody Issue patchedIssue) {
-        Issue issue = issueMap.get(hash);
-        if (issue == null) {
-            throw new IssueNotFoundException();
-        }
-        return issue.updateIssue(patchedIssue);
+        throwIfIssueNotFound(hash);
+
+        return issueRepository.replace(hash, issueRepository.findOne(hash).updateIssue(patchedIssue));
     }
 
     @GetMapping
     public Collection<Issue> getIssues(@RequestParam(value = "status", required = false) Issue.Status status) {
-        if (status == null) {
-            return issueMap.values();
-        }
-        return issueMap.values()
-                .stream()
-                .filter(entry -> entry.getStatus() == status)
+        return StreamSupport.stream(issueRepository.findAll(status).spliterator(), false)
                 .collect(Collectors.toList());
     }
 
-    @PostMapping(value = "{hash}/comments")
-    public void postComment(@RequestBody Comment comment, @PathVariable("hash") long hash) {
-        Issue issue = issueMap.get(hash);
-        if (issue == null) {
+    @PostMapping(value = "/{hash}/comments")
+    public void postComment(@RequestBody Comment comment,
+                            @PathVariable("hash") long hash) {
+        throwIfIssueNotFound(hash);
+
+        issueRepository.postComment(comment, hash);
+    }
+
+    private void throwIfIssueNotFound(long hash) {
+        if (issueRepository.exists(hash)) {
             throw new IssueNotFoundException();
         }
-        issue.addComment(comment);
     }
 
     @ResponseStatus(value = HttpStatus.NOT_FOUND, reason = "Issue not found")
---
+++ src/main/java/edu/xored/tracker/IssueRepositoryImpl.java
@@ -0,0 +1,101 @@
+package edu.xored.tracker;
+
+import java.util.ArrayList;
+import java.util.Map;
+import java.util.HashMap;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.StreamSupport;
+
+public class IssueRepositoryImpl implements IssueRepository {
+    private static Map<Long, Issue> issuesMap = new HashMap<>();
+
+    public <S extends Issue> S save(S issue) {
+        issue.setHash(getIssueId());
+        issuesMap.put(issue.getHash(), issue);
+
+        return issue;
+    }
+
+    public <S extends Issue> Iterable<S> save(Iterable<S> issues) {
+        if (issues == null) {
+            return null;
+        }
+
+        return StreamSupport.stream(issues.spliterator(), false)
+                .map(this::save)
+                .collect(Collectors.toList());
+    }
+
+    public Issue findOne(Long issueId) {
+        return issuesMap.getOrDefault(issueId, null);
+    }
+
+    public boolean exists(Long issueId) {
+        return issuesMap.containsKey(issueId);
+    }
+
+    public Iterable<Issue> findAll() {
+        return issuesMap.values();
+    }
+
+    public Iterable<Issue> findAll(Iterable<Long> issuesId) {
+        List<Issue> result = new ArrayList<>();
+        issuesId.forEach(issueId -> result.add(findOne(issueId)));
+
+        return result;
+    }
+
+    public long count() {
+        return (issuesMap == null)
+                ? 0
+                : issuesMap.size();
+    }
+
+    public void delete(Long issueId) {
+        delete(findOne(issueId));
+    }
+
+    public void delete(Issue issue) {
+        if (issue == null) {
+            return;
+        }
+
+        if (issuesMap.containsKey(issue.getHash())) {
+            issuesMap.remove(issue);
+        }
+    }
+
+    public void delete(Iterable<? extends Issue> issuesId) {
+        issuesId.forEach(this::delete);
+    }
+
+    public void deleteAll() {
+        issuesMap.clear();
+    }
+
+    public List<Issue> list() {
+        return issuesMap.entrySet().stream()
+                .map(Map.Entry::getValue)
+                .collect(Collectors.toList());
+    }
+
+    public Issue replace(Long hash, Issue issue) {
+        return issuesMap.replace(hash, issue);
+    }
+
+    public Iterable<Issue> findAll(Issue.Status status) {
+        return issuesMap.entrySet().stream()
+                .map(Map.Entry::getValue)
+                .filter(entry -> entry.getStatus() == status)
+                .collect(Collectors.toList());
+    }
+
+    public void postComment(Comment comment, long hash) {
+        findOne(hash).addComment(comment);
+    }
+
+    private long getIssueId() {
+        return issuesMap.size();
+    }
+}
