df18f61363e770c972f8479928dcfd1ff7194495
Dmitry Gerasimenko <astronautr@gmail.com>
2016-11-27 15:02:26 +0700

    Fix url in adress bar after closing modal dialog
    Also, use appropriate style at close dialog button (symbol).
    
    Card: #51

--- src/main/resources/static/directives/modalDialog.directive.js
+++ src/main/resources/static/directives/modalDialog.directive.js
@@ -32,7 +32,9 @@ function modalDialog() {
 			"<div class='ng-modal' ng-show='show'>\
 				<div class='ng-modal-overlay' ng-click='hideModal()'></div>\
 				<div class='ng-modal-dialog' ng-style='dialogStyle'>\
-					<div class='ng-modal-close' ng-click='hideModal()'>&#10060</div>\
+					<div class='ng-modal-close' ng-click='hideModal()'>\
+						<a href=' '>&#10060</a>\
+					</div>\
 					<div class='ng-modal-dialog-content' ng-transclude></div>\
 				</div>\
 			</div>"
--- src/main/resources/static/trackerWebApp.routes.js
+++ src/main/resources/static/trackerWebApp.routes.js
@@ -1,14 +1,18 @@
 angular
-  .module('trackerWebApp')
-  .config(function($routeProvider, $locationProvider) {
-    $routeProvider
-      .when('/issues/:hash', {
-        templateUrl: '/views/issueView.html',
-        controller: 'issueController'
-      })
-      .when('/issues/:hash/postcomment', {
-        templateUrl: '/views/postCommentView.html',
-        controller: 'postCommentController'
-      })
-      $locationProvider.html5Mode(true);
+	.module('trackerWebApp')
+	.config(function($routeProvider, $locationProvider) {
+	$routeProvider
+	.when('/issues/:hash', {
+		templateUrl: '/views/issueView.html',
+		controller: 'issueController'
+	})
+	.when('/issues/:hash/postcomment', {
+		templateUrl: '/views/postCommentView.html',
+		controller: 'postCommentController'
+	})
+	.otherwise({
+		templateUrl: '/views/allIssuesView.html.html',
+		controller: 'issuesController'
+	});
+	$locationProvider.html5Mode(true);
 });
