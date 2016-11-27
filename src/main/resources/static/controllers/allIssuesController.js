32661ba67496f04a160b6ba3f0224f83dd8f99b5
Declow <declow@mail.ru>
2016-11-28 01:52:22 +0700

    Change name of variables. Save status after open modelDialog
    Card: #72

--- src/main/resources/static/controllers/allIssuesController.js
+++ src/main/resources/static/controllers/allIssuesController.js
@@ -4,9 +4,9 @@ angular.module('trackerWebApp')
         $scope.itemsPerPage = 10;
         $scope.itemsPerPageSelects = [2, 5, 10, 20, 50];
         $scope.issues = [];
-        $scope.param = [];
-        $scope.changeParam = function (hash) {
-            $scope.param[hash] = !$scope.param[hash];
+        $scope.issueDisplay = [];
+        $scope.changeIssueDisplay = function (hash) {
+            $scope.issueDisplay[hash] = !$scope.issueDisplay[hash];
         }
         $scope.firstPage = function() {
             return $scope.currentPage == 0;
